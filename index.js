9683a6fa06d12da112dd94c2a2a24b08d9a2f4bc
mnaofficial <mna.official92@gmail.com>
2022-04-24 13:27:06 +0600

    pagination setup

--- index.js
+++ index.js
@@ -20,16 +20,22 @@ async function run() {
         const productCollection =client.db('emaJohn').collection('product');
 
         app.get('/product',async(req,res)=>{
+            const page=parseInt(req.query.page);
+            const size=parseInt(req.query.size);
             const query={};
             const cursor =productCollection.find(query);
-            const products=await cursor.toArray();
+            let products;
+            if(page || size){
+                products=await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                products=await cursor.toArray();
+            }
             res.send(products);
         });
 
         app.get('/productCount',async(req,res)=>{
-            const query={};
-            const cursor =productCollection.find(query);
-            const count=await cursor.count();
+            const count=await productCollection.estimatedDocumentCount();
             res.send({count}); 
         })
     }
